d06aa50df3b7078cbcdc98ec2199ff81b8c62a3a
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2022-07-03 17:16:44 -0300

    add br functionality

--- functions/update-covid-data-br/index.js
+++ functions/update-covid-data-br/index.js
@@ -1,6 +1,39 @@
+const {PubSub} = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
+
+const axios = require('axios').default;
+const url = 'https://api.brasil.io/dataset/covid19/caso/data/';
+const token = process.env.API_TOKEN;
+const topic = pubsub.topic('update-covid-data-middleware');
+
 exports.helloPubSub = (event, context) => {
-    const message = event.data
-        ? Buffer.from(event.data, 'base64').toString()
-        : 'Hello, World';
-    console.log(message);
+    axios.get(url, {headers: {'Authorization': 'Token ' + token}}).then(res => {
+        let arr = [];
+        const results = res.data.results;
+        if (results) {
+            arr = results.filter(r => r && r.city == null);
+        }
+
+        const totalConfirmed = arr.reduce((prev, curr) => prev + curr.confirmed, 0);
+        const totalDeaths = arr.reduce((prev, curr) => prev + curr.deaths, 0);
+
+        const messageObject = {
+            data: {
+                message: {
+                    country: 'BR',
+                    cases: totalConfirmed,
+                    deaths: totalDeaths
+                },
+            },
+        };
+
+        const messageBuffer = Buffer.from(JSON.stringify(messageObject), 'utf8');
+        try {
+            topic.publish(messageBuffer)
+                .then(() => console.log('Update event sent.'))
+                .catch(err => console.error('Error while sending update event: ', err));
+        } catch (err) {
+            console.error(err);
+        }
+    }).catch(err => console.error('Error while fetching data: ', err));
 };
