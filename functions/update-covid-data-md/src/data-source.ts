063839b8f6479d8a33169eea63a57d8ed6f392b2
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2022-07-03 14:55:40 -0300

    Fix conn

--- functions/update-covid-data-md/src/data-source.ts
+++ functions/update-covid-data-md/src/data-source.ts
@@ -2,10 +2,11 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { CountryData } from "./entity/CountryData"
 
+const hostport = (process.env.DB_HOST || '127.0.0.1:5432').split(':');
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: 5432,
+    host: hostport[0],
+    port: hostport[1],
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASS || 'postgres',
     database: process.env.DB_NAME || 'postgres',
