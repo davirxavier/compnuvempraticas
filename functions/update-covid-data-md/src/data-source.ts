7ff5372d0fb29600d5ffe1699265b73e1e496c93
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2022-07-03 15:00:02 -0300

    fix type err

--- functions/update-covid-data-md/src/data-source.ts
+++ functions/update-covid-data-md/src/data-source.ts
@@ -6,7 +6,7 @@ const hostport = (process.env.DB_HOST || '127.0.0.1:5432').split(':');
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: hostport[0],
-    port: hostport[1],
+    port: parseInt(hostport[1]),
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASS || 'postgres',
     database: process.env.DB_NAME || 'postgres',
