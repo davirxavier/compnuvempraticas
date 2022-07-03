b470509a1ba71405f67d08f9de8375d0ed2f1ab5
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2022-07-03 10:20:31 -0300

    init fns

---
+++ functions/update-covid-data-md/index.js
@@ -0,0 +1,10 @@
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+exports.helloPubSub = (event, context) => {
+    console.log(event);
+    console.log(context);
+};
