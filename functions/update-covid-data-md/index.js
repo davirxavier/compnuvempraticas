bdb30c899815604463c900d938de2c1116272cba
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2022-07-03 13:41:05 -0300

    remove import

--- functions/update-covid-data-md/index.js
+++ functions/update-covid-data-md/index.js
@@ -1,4 +1,31 @@
-const general = require('../general/general');
+const Knex = require("knex");
+
+const createTcpPool = async () => {
+    // Extract host and port from socket address
+    const dbSocketAddr = process.env.DB_HOST.split(':'); // e.g. '127.0.0.1:5432'
+
+    // Establish a connection to the database
+    return Knex({
+        client: 'pg',
+        connection: {
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            host: dbSocketAddr[0],
+            port: dbSocketAddr[1],
+        },
+        pool: { min: 0, max: 5 }
+    });
+};
+
+let conn = undefined;
+async function getConn() {
+    if (!conn) {
+        conn = await createTcpPool();
+    }
+
+    return conn;
+}
 
 /**
  * Triggered from a message on a Cloud Pub/Sub topic.
@@ -7,5 +34,5 @@ const general = require('../general/general');
  * @param {!Object} context Metadata for the event.
  */
 exports.helloPubSub = (event, context) => {
-    general.getConnection().then(conn => console.log(conn))
+    getConn().then(conn => console.log(conn))
 };
